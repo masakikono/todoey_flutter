e5af248a206a608d1eddea44972de155339859df
Masaki Kono <masaki19931015@gmail.com>
2022-07-10 22:56:48 +0900

    add: Textdecoration

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key? key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('this is a task'),
-      trailing: Checkbox(onChanged: null ,value: false),
+      title: Text(
+        'this is a task',
+        style: TextStyle(decoration: TextDecoration.lineThrough),
+      ),
+      trailing: TaskCheckBox(isChecked),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+
+  TaskCheckBox(this.checkboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (newValue) {
+        // setState(() {
+        //   isChecked = newValue;
+        // });
+      },
     );
   }
 }
